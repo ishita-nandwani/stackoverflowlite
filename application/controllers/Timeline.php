80bc5dbf2652150513cc934a5ac10bd89b2bee09
Khushboo Tolat <khushbootolat6@gmail.com>
2020-01-11 18:08:46 +0530

    Some files added

---
+++ application/controllers/Timeline.php
@@ -0,0 +1,10 @@
+<?php
+	defined('BASEPATH') or exit('Error');
+
+	class Timeline extends CI_Controller
+	{
+          function index(){
+               $this->load->view("home");
+          }
+     }
+?>
---
+++ application/views/footer.php
@@ -0,0 +1,7 @@
+<footer class="page-footer">
+     <div class="footer-copyright">
+          <div class="container">
+               <span class="right">Copyright &copy 2020 | All rights reserved</span>
+          </div>
+     </div>
+</footer>
